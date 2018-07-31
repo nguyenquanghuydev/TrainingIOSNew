6e75de8503a252773c314cbf99b62e1f5d317479
Nguyen Huy <nguyenquanghuydev@gmail.com>
2018-07-31 14:52:51 +0700

    update2

---
+++ TrainingExe1.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+print("************")
+for i in 0...10 {
+    print("*          *")
+}
+print("************")
